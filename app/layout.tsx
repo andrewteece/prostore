a8f3e958a2052a266030f8ef75cc24301f8dcafb
andrewteece <andrewteece@gmail.com>
2025-04-08 17:50:45 -0400

    install typescript and customize layout

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+//import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: "Prostore",
+  description: "A modern store built with Next.js, ShadCN, and Prisma.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
