839aae2a3a1d221458d26ff5bcafefd2685daf81
andrewteece <andrewteece@gmail.com>
2025-04-09 08:38:51 -0400

    update page and add images

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google'
 import "@/assets/styles/globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
