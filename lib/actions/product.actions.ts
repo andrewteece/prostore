c154393340d44a5b51ee34e3c447152a18ad0bce
andrewteece <andrewteece@gmail.com>
2025-04-11 11:38:39 -0400

    update index and product.actions

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,13 +2,14 @@
 
 import { PrismaClient } from '@/lib/generated/prisma';
 import { convertToPlainObject } from '../utils';
+import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get the latest products
 export async function getLatestProducts() {
     const prisma = new PrismaClient();
   
     const data = await prisma.product.findMany({
-      take: 4,
+      take: LATEST_PRODUCTS_LIMIT,
       orderBy: { createdAt: 'desc' },
     });
   
