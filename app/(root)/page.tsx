1db0dc12def288cf9b1a1c2714a7cc96458a4ac6
andrewteece <andrewteece@gmail.com>
2025-04-11 08:36:00 -0400

    limit data for products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,10 +5,16 @@ import sampleData from "@/db/sample-data";
 
 const HomePage =  () => {
   return (
-    <div className='space-y-8'>
+    
+    <>
       {/* <h2 className='h2-bold'>Latest Products</h2> */}
-      <ProductList title='Newest Arrivals' data={sampleData.products} />
-    </div>
+      <ProductList 
+      data={sampleData.products} 
+      title='Newest Arrivals'
+      limit={4}
+      />
+      </>
+    
   );
 
 };
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,10 +1,12 @@
-const ProductList = ({ data, title }: { data: any; title?: string }) => {
+const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?:number }) => {
+    const limitedData = limit ? data.slice(0, limit) : data
+
     return (
         <div className="my-10">
             <h2 className="h2-bold mb-4">{title}</h2>
             {data.length > 0 ? (
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-            {data.map((product: any) => (
+            {limitedData.map((product: any) => (
               <div>{product.name}</div>
             ))}
           </div>
