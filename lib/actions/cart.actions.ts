cc55befd58259e41709c5677585fa88b85d35057
andrewteece <andrewteece@gmail.com>
2025-04-18 07:42:31 -0400

    price calc and add to database

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -9,7 +9,24 @@ import { cartItemSchema, insertCartSchema } from "../validator";
 import { prisma } from "@/db/prisma";
 import { CartItem } from "@/types"
 import { Prisma } from "@prisma/client";
-import { convertToPlainObject } from "../utils";
+import { convertToPlainObject, round2 } from "../utils";
+
+// Calculate cart price based on items
+const calcPrice = (items: z.infer<typeof cartItemSchema>[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
+
 
 // Add item to cart in database
 export const addItemToCart = async (data: z.infer<typeof cartItemSchema>) => {
@@ -30,15 +47,31 @@ export const addItemToCart = async (data: z.infer<typeof cartItemSchema>) => {
     });
     if (!product) throw new Error('Product not found');
 
-    // Testing
-    console.log({
-      'Session Cart ID': sessionCartId,
-      'User ID': userId,
-      'Item Requested': item,
-      'Product Found': product,
-      cart: cart,
-    });
+    if (!cart) {
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
 
+      // console.log(newCart);
+
+      // Add to database
+      await prisma.cart.create({
+        data: newCart,
+      });
+    
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+    
+      return {
+        success: true,
+        message: 'Item added to cart',
+      };
+    }
+    
     return {
       success: true,
       message: 'Testing Cart',
