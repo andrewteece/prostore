c0c5944557025d13992c57495bdb6227d4083686
andrewteece <andrewteece@gmail.com>
2025-04-19 08:36:56 -0400

    remove item from cart and update page

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,15 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { Toaster, toast } from 'sonner';
-import { ToasterProps } from "sonner";
+//import { ToasterProps } from "sonner";
 //import { round2 } from '@/lib/utils'
-import { CartItem } from "@/types"
-import { Plus } from "lucide-react";
+import { Cart, CartItem } from "@/types"
+import { Plus, Minus } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
 
-const AddToCart = ({ item }: { item: Omit<CartItem, 'cartId'> }) => {
+// const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem > }) => {
+
+const AddToCart = ({ cart, item }: { cart?: Cart; item: Omit<CartItem, 'cartId'> }) => {
 
   const router = useRouter();
   // const toast = useToast();
@@ -30,7 +32,7 @@ const AddToCart = ({ item }: { item: Omit<CartItem, 'cartId'> }) => {
      });
       return;
     }
-  
+     // Handle success add to cart
     toast.success("Item added", {
       description: `${item.name} added to the cart`,
       action: (
@@ -44,12 +46,43 @@ const AddToCart = ({ item }: { item: Omit<CartItem, 'cartId'> }) => {
     });
   };
 
-    return (
-      <Button className='w-full' type='button' onClick={handleAddToCart}>
-    <Plus />
-    Add to cart
-  </Button>
-    )
+  // Remove item from cart
+const handleRemoveFromCart = async () => {
+  const res = await removeItemFromCart(item.productId);
+
+  toast({
+    variant: res.success ? 'default' : 'destructive',
+    description: res.message,
+  });
+};
+
+// Check if item is in cart 
+const existItem =
+  cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+        <Minus className='w-4 h-4' />
+      </Button>
+      <span className='px-2'>{existItem.qty}</span>
+      <Button type='button' variant='outline' onClick={handleAddToCart}>
+        <Plus className='w-4 h-4' />
+      </Button>
+    </div>
+  ) : (
+    <Button className='w-full' type='button' onClick={handleAddToCart}>
+      <Plus className='w-4 h-4' />
+      Add to cart
+    </Button>
+  );
+
+  //   return (
+  //     <Button className='w-full' type='button' onClick={handleAddToCart}>
+  //   <Plus />
+  //   Add to cart
+  // </Button>
+  //   )
   };
   
   export default AddToCart;
