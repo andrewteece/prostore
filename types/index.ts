22917dccc887acba7396b6b5d1ae08006c924bd1
andrewteece <andrewteece@gmail.com>
2025-04-20 07:54:25 -0400

    update types index and user.actions

--- types/index.ts
+++ types/index.ts
@@ -3,9 +3,10 @@ import {
   cartItemSchema,
   insertCartSchema,
   insertProductSchema,
+  shippingAddressSchema
 } from '@/lib/validator'
 
-
+// product types
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     createdAt: Date;
@@ -13,5 +14,19 @@ export type Product = z.infer<typeof insertProductSchema> & {
     numReviews: number;
   };
 
+
+// cart types
   export type Cart = z.infer<typeof insertCartSchema>;
   export type CartItem = z.infer<typeof cartItemSchema>; 
+
+
+// shipping types
+  export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+  export const shippingAddressDefaultValues = {
+    fullName: 'John Doe',
+    streetAddress: '123 Main St',
+    city: 'Anytown',
+    postalCode: '12345',
+    country: 'USA',
+  };
