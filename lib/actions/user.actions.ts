2261af58da3890d799fc3a3d1e97fb54bdb0d9ba
andrewteece <andrewteece@gmail.com>
2025-04-20 09:34:49 -0400

    add payment schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { auth, signIn, signOut } from "@/auth";
-import { signInFormSchema, signUpFormSchema, shippingAddressSchema, updateUserSchema } from "../validator";
+import { signInFormSchema, signUpFormSchema, shippingAddressSchema, updateUserSchema, paymentMethodSchema } from "../validator";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
@@ -111,6 +111,33 @@ export async function updateUserAddress(data: ShippingAddress) {
     await prisma.user.update({
       where: { id: currentUser.id },
       data: { address },
+    });
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user.id! },
+    });
+    if (!currentUser) throw new Error('User not found');
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
     });
 
     return {
