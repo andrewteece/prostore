fb9392323908cfe5bc649db1d1ce60fef74c3e4d
andrewteece <andrewteece@gmail.com>
2025-04-24 07:15:56 -0400

    update code admin overview

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,4 +1,24 @@
+import { auth } from '@/auth';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { getOrderSummary } from '@/lib/actions/order.actions';
+import { formatCurrency, formatDateTime, formatNumber } from '@/lib/utils';
+import { BadgeDollarSign, Barcode, CreditCard, Users } from 'lucide-react';
 import { Metadata } from 'next';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow.
+} from '@/components/ui/table';
+import Link from 'next/link';
+
 
 export const metadata: Metadata = {
   title: 'Admin Dashboard',
