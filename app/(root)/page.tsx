0de61b1e56f64f5be286f4d7b7671551a1a88236
andrewteece <andrewteece@gmail.com>
2025-04-28 08:29:37 -0400

    remove h2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,15 @@
 //import { Button } from "@/components/ui/button";
-import ProductList from "@/components/shared/product/product-list";
+import ProductList from '@/components/shared/product/product-list';
 // import sampleData from "@/db/sample-data";
-import { getLatestProducts } from "@/lib/actions/product.actions";
-
+import { getLatestProducts } from '@/lib/actions/product.actions';
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
 
   return (
     <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
-      <ProductList data={latestProducts} title='Newest Arrivals'  />
+      {/* <h2 className='h2-bold'>Latest Products</h2> */}
+      <ProductList data={latestProducts} title='Newest Arrivals' />
     </div>
   );
 };
