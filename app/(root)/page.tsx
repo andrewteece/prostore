f2a583b18603c2801eec39041ee373e2f7d37f99
andrewteece <andrewteece@gmail.com>
2025-04-28 09:15:25 -0400

    setup product carousel and add to page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,24 @@
 //import { Button } from "@/components/ui/button";
 import ProductList from '@/components/shared/product/product-list';
-// import sampleData from "@/db/sample-data";
-import { getLatestProducts } from '@/lib/actions/product.actions';
+
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from '@/lib/actions/product.actions';
+import { ProductCarousel } from '@/components/shared/product/product-carousel';
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
-    <div className='space-y-8'>
+    <>
       {/* <h2 className='h2-bold'>Latest Products</h2> */}
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title='Newest Arrivals' />
-    </div>
+    </>
   );
 };
 
