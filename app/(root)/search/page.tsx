a78cae493349fb16b01a03e74bf26f1f3393c2a1
andrewteece <andrewteece@gmail.com>
2025-04-28 14:58:57 -0400

    refactor getFilterUrl

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -7,6 +7,29 @@ import {
 } from '@/lib/actions/product.actions';
 import Link from 'next/link';
 
+const prices = [
+  {
+    name: '$1 to $50',
+    value: '1-50',
+  },
+  {
+    name: '$51 to $100',
+    value: '51-100',
+  },
+  {
+    name: '$101 to $200',
+    value: '101-200',
+  },
+  {
+    name: '$201 to $500',
+    value: '201-500',
+  },
+  {
+    name: '$501 to $1000',
+    value: '501-1000',
+  },
+];
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string;
@@ -26,6 +49,33 @@ const SearchPage = async (props: {
     page = '1',
   } = await props.searchParams;
 
+  const categories = await getAllCategories();
+
+  // construct filter url
+  const getFilterUrl = ({
+    c,
+    p,
+    s,
+    r,
+    pg,
+  }: {
+    c?: string;
+    p?: string;
+    s?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (s) params.sort = s;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   // Get products
   const products = await getAllProducts({
     category,
@@ -44,7 +94,35 @@ const SearchPage = async (props: {
 
   return (
     <div className='grid md:grid-cols-5 md:gap-5'>
-      <div className='filter-links'>{/* FILTERS */}</div>
+      <div className='filter-links'>
+        {/* Category Links */}
+        <div className='text-xl mt-3 mb-2'>Department</div>
+        <div>
+          <ul className='space-y-1'>
+            <li>
+              <Link
+                className={`${
+                  ('all' === category || '' === category) && 'font-bold'
+                }`}
+                href={getFilterUrl({ c: 'all' })}
+              >
+                Any
+              </Link>
+            </li>
+            {categories.map((x) => (
+              <li key={x.category}>
+                <Link
+                  className={`${x.category === category && 'font-bold'}`}
+                  href={getFilterUrl({ c: x.category })}
+                >
+                  {x.category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
       <div className='md:col-span-4 space-y-4'>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
           {products!.data.length === 0 && <div>No product found</div>}
