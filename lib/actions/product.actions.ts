99fea2a5b733e8500203d67a5a20ff8cd23b7115
andrewteece <andrewteece@gmail.com>
2025-04-28 16:03:07 -0400

    add sorting

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -75,13 +75,20 @@ export async function getAllProducts({
 
   // fetch products
   const data = await prisma.product.findMany({
-    orderBy: { createdAt: 'desc' },
     where: {
       ...queryFilter,
       ...categoryFilter,
       ...priceFilter,
       ...ratingFilter,
     },
+    orderBy:
+      sort === 'lowest'
+        ? { price: 'asc' }
+        : sort === 'highest'
+        ? { price: 'desc' }
+        : sort === 'rating'
+        ? { rating: 'desc' }
+        : { createdAt: 'desc' },
     skip: (page - 1) * limit,
     take: limit,
   });
